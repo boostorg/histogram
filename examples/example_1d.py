916e687376d28104988fa6b7c2af0c3d7ee0fb6a
Hans Dembinski <hans.dembinski@icecube.wisc.edu>
2016-04-10 17:04:04 -0400

    adding examples, bug-fix in cmake

---
+++ examples/example_1d.py
@@ -0,0 +1,18 @@
+import histogram as hg
+import numpy as np
+import matplotlib.pyplot as plt
+
+h = hg.histogram(hg.regular_axis(10, -3, 3))
+h.fill(np.random.randn(1000))
+
+bins = h.axis(0).bins
+
+x = np.array(h.axis(0)) # axis instances behave like sequences
+y = np.asarray(h)       # creates a view (no copy involved)
+y = y[:bins]            # cut off underflow/overflow bins
+y = np.append(y, [0])   # append a zero because matplotlib's plot(...) is weird
+
+plt.plot(x, y, drawstyle="steps-post")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
---
+++ examples/example_2d.py
@@ -0,0 +1,20 @@
+import histogram as hg
+import numpy as np
+import matplotlib.pyplot as plt
+
+h = hg.histogram(hg.regular_axis(10, -3, 3, uoflow=False),
+                 hg.regular_axis(10, -3, 3, uoflow=False))
+xy = np.random.randn(2000).reshape((1000, 2))
+xy[:,1] *= 0.5
+h.fill(xy)
+
+bins = h.axis(0).bins
+
+x = np.array(h.axis(0)) # axis instances behave like sequences
+y = np.array(h.axis(1))
+z = np.asarray(h)       # creates a view (no copy involved)
+
+plt.pcolor(x, y, z.T)
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
