67b2220b523a36f344505c4603b2256e9034dbfe
Hans Dembinski <hans.dembinski@gmail.com>
2016-07-17 12:10:42 -0400

    getting rid of boost::assign

--- test/zero_suppression_test.cpp
+++ test/zero_suppression_test.cpp
@@ -8,9 +8,7 @@
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_tools.hpp>
 #include <boost/histogram/detail/zero_suppression.hpp>
-#include <boost/assign/std/vector.hpp>
 #include <iostream>
-using namespace boost::assign;
 using namespace boost::histogram::detail;
 namespace tt = boost::test_tools;
 
@@ -29,8 +27,7 @@ print(const std::vector<T>& v) {
 
 BOOST_AUTO_TEST_CASE( codec_no_zero )
 {
-    std::vector<unsigned> a, b, c(3, 0);
-    a += 1, 1, 1;
+    std::vector<unsigned> a{1, 1, 1}, b, c(3, 0);
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()));
     EQUAL_VECTORS(a, b);
     zero_suppression_decode<unsigned>(&c[0], c.size(), b);
@@ -48,16 +45,13 @@ BOOST_AUTO_TEST_CASE( codec_empty )
 
 BOOST_AUTO_TEST_CASE( codec_zero_0a )
 {
-    std::vector<uint8_t> a, b;
-    a += 0;
+    std::vector<uint8_t> a{0}, b;
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()) == false);
 }
 
 BOOST_AUTO_TEST_CASE( codec_zero_1a )
 {
-    std::vector<uint8_t> a, b, c, d(3, 0);
-    a += 1, 0, 0;
-    c += 1, 0, 2;
+    std::vector<uint8_t> a{1, 0, 0}, b, c{1, 0, 2}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -66,9 +60,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_1a )
 
 BOOST_AUTO_TEST_CASE( codec_zero_2a )
 {
-    std::vector<uint8_t> a, b, c, d(3, 0);
-    a += 0, 0, 1;
-    c += 0, 2, 1;
+    std::vector<uint8_t> a{0, 0, 1}, b, c{0, 2, 1}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -77,9 +69,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_2a )
 
 BOOST_AUTO_TEST_CASE( codec_zero_3a )
 {
-    std::vector<uint8_t> a, b, c, d(2, 0);
-    a += 0, 0;
-    c += 0, 2;
+    std::vector<uint8_t> a{0, 0}, b, c{0, 2}, d(2, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -88,16 +78,13 @@ BOOST_AUTO_TEST_CASE( codec_zero_3a )
 
 BOOST_AUTO_TEST_CASE( codec_zero_0b )
 {
-    std::vector<unsigned> a, b;
-    a += 0;
+    std::vector<unsigned> a{0}, b;
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()) == false);
 }
 
 BOOST_AUTO_TEST_CASE( codec_zero_1b )
 {
-    std::vector<unsigned> a, b, c, d(3, 0);
-    a += 1, 0, 0;
-    c += 1, 0, 2;
+    std::vector<unsigned> a{1, 0, 0}, b, c{1, 0, 2}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<unsigned>(&d[0], d.size(), b);
@@ -106,9 +93,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_1b )
 
 BOOST_AUTO_TEST_CASE( codec_zero_2b )
 {
-    std::vector<unsigned> a, b, c, d(3, 0);
-    a += 0, 0, 1;
-    c += 0, 2, 1;
+    std::vector<unsigned> a{0, 0, 1}, b, c{0, 2, 1}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<unsigned>(&d[0], d.size(), b);
@@ -117,9 +102,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_2b )
 
 BOOST_AUTO_TEST_CASE( codec_zero_3b )
 {
-    std::vector<unsigned> a, b, c, d(2, 0);
-    a += 0, 0;
-    c += 0, 2;
+    std::vector<unsigned> a{0, 0}, b, c{0, 2}, d(2, 0);
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<unsigned>(&d[0], d.size(), b);
@@ -128,16 +111,13 @@ BOOST_AUTO_TEST_CASE( codec_zero_3b )
 
 BOOST_AUTO_TEST_CASE( codec_zero_0c )
 {
-    std::vector<uint64_t> a, b;
-    a += 0;
+    std::vector<uint64_t> a{0}, b;
     BOOST_CHECK(zero_suppression_encode<uint64_t>(b, &a[0], a.size()) == false);
 }
 
 BOOST_AUTO_TEST_CASE( codec_zero_1c )
 {
-    std::vector<uint64_t> a, b, c, d(3, 0);
-    a += 1, 0, 0;
-    c += 1, 0, 2;
+    std::vector<uint64_t> a{1, 0, 0}, b, c{1, 0, 2}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<uint64_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint64_t>(&d[0], d.size(), b);
@@ -146,9 +126,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_1c )
 
 BOOST_AUTO_TEST_CASE( codec_zero_2c )
 {
-    std::vector<uint64_t> a, b, c, d(3, 0);
-    a += 0, 0, 1;
-    c += 0, 2, 1;
+    std::vector<uint64_t> a{0, 0, 1}, b, c{0, 2, 1}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<uint64_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint64_t>(&d[0], d.size(), b);
@@ -157,9 +135,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_2c )
 
 BOOST_AUTO_TEST_CASE( codec_zero_3c )
 {
-    std::vector<uint64_t> a, b, c, d(2, 0);
-    a += 0, 0;
-    c += 0, 2;
+    std::vector<uint64_t> a{0, 0}, b, c{0, 2}, d(2, 0);
     BOOST_CHECK(zero_suppression_encode<uint64_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint64_t>(&d[0], d.size(), b);
@@ -168,16 +144,13 @@ BOOST_AUTO_TEST_CASE( codec_zero_3c )
 
 BOOST_AUTO_TEST_CASE( codec_zero_0d )
 {
-    std::vector<wtype> a, b;
-    a += 0;
+    std::vector<wtype> a{0}, b;
     BOOST_CHECK(zero_suppression_encode<wtype>(b, &a[0], a.size()) == false);
 }
 
 BOOST_AUTO_TEST_CASE( codec_zero_1d )
 {
-    std::vector<wtype> a, b, c, d(3, 0);
-    a += 1, 0, 0;
-    c += 1, 0, 2;
+    std::vector<wtype> a{1, 0, 0}, b, c{1, 0, 2}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<wtype>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<wtype>(&d[0], d.size(), b);
@@ -186,9 +159,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_1d )
 
 BOOST_AUTO_TEST_CASE( codec_zero_2d )
 {
-    std::vector<wtype> a, b, c, d(3, 0);
-    a += 0, 0, 1;
-    c += 0, 2, 1;
+    std::vector<wtype> a{0, 0, 1}, b, c{0, 2, 1}, d(3, 0);
     BOOST_CHECK(zero_suppression_encode<wtype>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<wtype>(&d[0], d.size(), b);
@@ -197,9 +168,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_2d )
 
 BOOST_AUTO_TEST_CASE( codec_zero_3d )
 {
-    std::vector<wtype> a, b, c, d(2, 0);
-    a += 0, 0;
-    c += 0, 2;
+    std::vector<wtype> a{0, 0}, b, c{0, 2}, d(2, 0);
     BOOST_CHECK(zero_suppression_encode<wtype>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<wtype>(&d[0], d.size(), b);
@@ -208,9 +177,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_3d )
 
 BOOST_AUTO_TEST_CASE( codec_zero_4 )
 {
-    std::vector<unsigned> a, b, c, d(5, 0);
-    a += 0, 0, 0, 1, 0;
-    c += 0, 3, 1, 0, 1;
+    std::vector<unsigned> a{0, 0, 0, 1, 0}, b, c{0, 3, 1, 0, 1}, d(5, 0);
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<unsigned>(&d[0], d.size(), b);
@@ -219,9 +186,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_4 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_5 )
 {
-    std::vector<unsigned> a, b, c, d(5, 0);
-    a += 0, 1, 0, 0, 0;
-    c += 0, 1, 1, 0, 3;
+    std::vector<unsigned> a{0, 1, 0, 0, 0}, b, c{0, 1, 1, 0, 3}, d(5, 0);
     BOOST_CHECK(zero_suppression_encode<unsigned>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<unsigned>(&d[0], d.size(), b);
@@ -230,8 +195,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_5 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_6 )
 {
-    std::vector<uint8_t> a(255, 0), b, c, d(255, 0);
-    c += 0, 255;
+    std::vector<uint8_t> a(255, 0), b, c{0, 255}, d(255, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -240,8 +204,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_6 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_7 )
 {
-    std::vector<uint8_t> a(256, 0), b, c, d(256, 0);
-    c += 0, 0;
+    std::vector<uint8_t> a(256, 0), b, c{0, 0}, d(256, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -250,8 +213,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_7 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_8 )
 {
-    std::vector<uint8_t> a(257, 0), b, c, d(257, 0);
-    c += 0, 0, 0, 1;
+    std::vector<uint8_t> a(257, 0), b, c{0, 0, 0, 1}, d(257, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -260,8 +222,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_8 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_9 )
 {
-    std::vector<uint8_t> a(1000, 0), b, c, d(1000, 0);
-    c += 0, 0, 0, 0, 0, 0, 0, 232;
+    std::vector<uint8_t> a(1000, 0), b, c{0, 0, 0, 0, 0, 0, 0, 232}, d(1000, 0);
     BOOST_CHECK(zero_suppression_encode<uint8_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint8_t>(&d[0], d.size(), b);
@@ -270,8 +231,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_9 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_10 )
 {
-    std::vector<uint16_t> a(1000, 0), b, c, d(1000, 0);
-    c += 0, 1000;
+    std::vector<uint16_t> a(1000, 0), b, c{0, 1000}, d(1000, 0);
     BOOST_CHECK(zero_suppression_encode<uint16_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint16_t>(&d[0], d.size(), b);
@@ -280,8 +240,7 @@ BOOST_AUTO_TEST_CASE( codec_zero_10 )
 
 BOOST_AUTO_TEST_CASE( codec_zero_11 )
 {
-    std::vector<uint16_t> a(65536, 0), b, c, d(65536, 0);
-    c += 0, 0;
+    std::vector<uint16_t> a(65536, 0), b, c{0, 0}, d(65536, 0);
     BOOST_CHECK(zero_suppression_encode<uint16_t>(b, &a[0], a.size()));
     EQUAL_VECTORS(b, c);
     zero_suppression_decode<uint16_t>(&d[0], d.size(), b);
