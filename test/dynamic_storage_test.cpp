c18e0c2848f6e295ff12263659c2e58ef3de18e5
Hans Dembinski <hans.dembinski@gmail.com>
2016-07-23 18:17:21 -0400

    better test

--- test/dynamic_storage_test.cpp
+++ test/dynamic_storage_test.cpp
@@ -35,70 +35,50 @@ BOOST_AUTO_TEST_CASE(wtype_streamer)
     BOOST_CHECK_EQUAL(os.str(), std::string("(1.2,2.3)"));
 }
 
-BOOST_AUTO_TEST_CASE(dynamic_storage_grow_1)
+BOOST_AUTO_TEST_CASE(dynamic_storage_increase_and_grow)
 {
-    double x = 1.0;
-    dynamic_storage n(1), p(1);
-    n.increase(0);
-    for (unsigned i = 0; i < 100; ++i) {
-        p = n;
-        n += p;
-        x += x;
+    dynamic_storage n(1);
+    double v = 0;
+    for (unsigned b = 1; b <= 8; b *= 2) {
+        uint64_t x = 0;
+        for (unsigned t = 0; t < (8 * b); ++t) { x <<= 1; ++x; }
+        void* buf = const_cast<void*>(n.data());
+        switch (b) {
+            case 1: (static_cast<uint8_t*>(buf))[0] = x;
+            case 2: (static_cast<uint16_t*>(buf))[0] = x;
+            case 4: (static_cast<uint32_t*>(buf))[0] = x;
+            case 8: (static_cast<uint64_t*>(buf))[0] = x;
+        }
+        v = x;
+        BOOST_CHECK_EQUAL(n.value(0), v);
         n.increase(0);
-        ++x;
-        BOOST_CHECK_EQUAL(n.value(0), x);
-        BOOST_CHECK_EQUAL(n.variance(0), x);
+        ++v;
+        BOOST_CHECK_EQUAL(n.value(0), v);
     }
-}
-
-BOOST_AUTO_TEST_CASE(dynamic_storage_grow_2)
-{
-    dynamic_storage n(1), p(1);
     n.increase(0);
-    for (unsigned i = 0; i < 100; ++i) {
-        p = n;
-        n += p;
-        dynamic_storage a(1);
-        a.increase(0, 0.0); // converts to wtype
-        a = n;
-        BOOST_CHECK_EQUAL(a.value(0), n.value(0));
-        BOOST_CHECK_EQUAL(a.variance(0), n.variance(0));
-    }
+    ++v;
+    BOOST_CHECK_EQUAL(n.value(0), v);
 }
 
-BOOST_AUTO_TEST_CASE(dynamic_storage_grow_3)
-{
-    dynamic_storage n(1), p(1);
-    n.increase(0);
-    for (unsigned i = 0; i < 100; ++i) {
-        p = n;
-        n += p;
-        dynamic_storage a(1);
-        a += n;
-        a.increase(0, 0.0); // converts to wtype
-        BOOST_CHECK_EQUAL(a.value(0), n.value(0));
-        BOOST_CHECK_EQUAL(a.variance(0), n.variance(0));
-    }
-}
-
-BOOST_AUTO_TEST_CASE(dynamic_storage_add_with_growth)
+BOOST_AUTO_TEST_CASE(dynamic_storage_add_and_grow)
 {
     dynamic_storage a(1), b(1);
     a.increase(0);
-    for (unsigned i = 0; i < 100; ++i)
+    b.increase(0, 1.0);
+    double x = 1.0;
+    for (unsigned i = 0; i < 100; ++i) {
+        b += a;
         a += a;
-    b.increase(0);
-    b += a;
-    BOOST_CHECK_EQUAL(b.value(0), a.value(0) + 1.0);
-}
-
-BOOST_AUTO_TEST_CASE(dynamic_storage_add_wtype)
-{
-    dynamic_storage a(1), b(1);
-    a.increase(0, 1.0);
-    b.increase(0);
-    a += b;
-    BOOST_CHECK_EQUAL(a.value(0), b.value(0) + 1.0);
+        x += x;
+        BOOST_CHECK_EQUAL(a.value(0), x);
+        BOOST_CHECK_EQUAL(a.variance(0), x);
+        BOOST_CHECK_EQUAL(b.value(0), x);
+        BOOST_CHECK_EQUAL(b.variance(0), x);        
+        dynamic_storage c(1);
+        c += a;
+        BOOST_CHECK_EQUAL(c.value(0), x);
+        BOOST_CHECK_EQUAL(c.variance(0), x);        
+    }
 }
 
 BOOST_AUTO_TEST_CASE(dynamic_storage_equality)
@@ -111,83 +91,40 @@ BOOST_AUTO_TEST_CASE(dynamic_storage_equality)
     BOOST_CHECK(a == c);
 }
 
-BOOST_AUTO_TEST_CASE(convert_static_storage_1)
-{
-    dynamic_storage a(2), b(2);
-    static_storage<uint8_t> c(2);
-    c.increase(0);
-    for (unsigned i = 0; i < 7; ++i)
-        c += c;
-    a = c;
-    BOOST_CHECK(a == c);
-    BOOST_CHECK(c == a);
-    BOOST_CHECK_EQUAL(a.value(0), 128);
-    BOOST_CHECK_EQUAL(a.value(1), 0);
-    b = std::move(c);
-    BOOST_CHECK(a == b);
+template <typename T>
+void convert_static_storage_test() {
+    dynamic_storage a(2), b(2), c(2);
+    static_storage<T> s(2);
+    a.increase(0);
+    b.increase(0, 1.0);
+    s.increase(0);
+    for (unsigned i = 0; i < (sizeof(T) - 1); ++i)
+        s += s;
+    a = s;
+    b = s;
+    BOOST_CHECK(a == s);
+    BOOST_CHECK(s == a);
+    BOOST_CHECK(b == s);
+    BOOST_CHECK(s == b);
+    a = dynamic_storage(2);
+    b = dynamic_storage(2);
+    a += s;
+    b += s;
+    BOOST_CHECK(a == s);
+    BOOST_CHECK(s == a);
+    BOOST_CHECK(b == s);
+    BOOST_CHECK(s == b);    
+    dynamic_storage d;
+    d = std::move(s);
+    BOOST_CHECK(a == d);
 }
 
-BOOST_AUTO_TEST_CASE(convert_static_storage_2)
+BOOST_AUTO_TEST_CASE(convert_static_storage)
 {
-    dynamic_storage a(2), b(2);
-    static_storage<uint16_t> c(2);
-    c.increase(0);
-    for (unsigned i = 0; i < 15; ++i)
-        c += c;
-    a = c;
-    BOOST_CHECK(a == c);
-    BOOST_CHECK(c == a);
-    BOOST_CHECK_EQUAL(a.value(0), 32768);
-    BOOST_CHECK_EQUAL(a.value(1), 0);
-    b = std::move(c);
-    BOOST_CHECK(a == b);
-}
-
-BOOST_AUTO_TEST_CASE(convert_static_storage_3)
-{
-    dynamic_storage a(2), b(2);
-    static_storage<uint32_t> c(2);
-    c.increase(0);
-    for (unsigned i = 0; i < 31; ++i)
-        c += c;
-    a = c;
-    BOOST_CHECK(a == c);
-    BOOST_CHECK_EQUAL(a.value(0), 2147483648);
-    BOOST_CHECK_EQUAL(a.value(1), 0);
-    b = std::move(c);
-    BOOST_CHECK(a == b);
-}
-
-BOOST_AUTO_TEST_CASE(convert_static_storage_4)
-{
-    dynamic_storage a(2), b(2);
-    static_storage<uint64_t> c(2);
-    c.increase(0);
-    for (unsigned i = 0; i < 63; ++i)
-        c += c;
-    a = c;
-    BOOST_CHECK(a == c);
-    BOOST_CHECK_EQUAL(a.value(0), 9223372036854775808lu);
-    BOOST_CHECK_EQUAL(a.value(1), 0);
-    b = std::move(c);
-    BOOST_CHECK(a == b);
-}
-
-BOOST_AUTO_TEST_CASE(convert_static_storage_5)
-{
-    dynamic_storage a(2), b(2);
-    a.increase(0, 0.0);
-    b.increase(0, 0.0);
-    static_storage<uint64_t> c(2);
-    c.increase(0);
-    for (unsigned i = 0; i < 63; ++i)
-        c += c;
-    a = c;
-    BOOST_CHECK(a == c);
-    BOOST_CHECK_EQUAL(a.value(0), 9223372036854775808lu);
-    BOOST_CHECK_EQUAL(a.value(1), 0);
-    b = std::move(c);
-    BOOST_CHECK(a == b);
+    convert_static_storage_test<uint8_t>();
+    convert_static_storage_test<uint16_t>();
+    convert_static_storage_test<uint32_t>();
+    convert_static_storage_test<uint64_t>();
 }
 
 BOOST_AUTO_TEST_CASE(convert_static_storage_6)
