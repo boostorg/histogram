f8ad3052b87f1ba2bda97c6b155c929ed766e6f7
hans.dembinski@gmail.com <hdembins@mpi-hd.mpg.de>
2017-02-01 18:41:13 +0000

    more coverage

--- test/detail_test.cpp
+++ test/detail_test.cpp
@@ -14,33 +14,15 @@
 #include <algorithm>
 using namespace boost::histogram::detail;
 
-// bool operator==(const buffer& a, const buffer& b)
-// {
-//     if (!(a.size() == b.size() &&
-//           a.id() == b.id() &&
-//           a.depth() == b.depth()))
-//         return false;
-//     switch (a.id()) {
-//         case -1: return std::equal(&a.at<weight>(0),
-//                                    &a.at<weight>(a.size()),
-//                                    &b.at<weight>(0));
-//         case 0: return true;
-//         case 1: return std::equal(&a.at<uint8_t>(0),
-//                                   &a.at<uint8_t>(a.size()),
-//                                   &b.at<uint8_t>(0));
-//         case 2: return std::equal(&a.at<uint16_t>(0),
-//                                   &a.at<uint16_t>(a.size()),
-//                                   &b.at<uint16_t>(0));
-//         case 3: return std::equal(&a.at<uint32_t>(0),
-//                                   &a.at<uint32_t>(a.size()),
-//                                   &b.at<uint32_t>(0));
-//         case 4: return std::equal(&a.at<uint64_t>(0),
-//                                   &a.at<uint64_t>(a.size()),
-//                                   &b.at<uint64_t>(0));
-//     }
-//     BOOST_ASSERT(!"never reach this");
-//     return false;
-// }
+BOOST_AUTO_TEST_CASE(weight_test)
+{
+    BOOST_CHECK(weight(0) == weight());
+    weight w(1);
+    BOOST_CHECK(w == weight(1));
+    BOOST_CHECK(w != weight());
+    BOOST_CHECK(1 == w);
+    BOOST_CHECK(w == 1);
+}
 
 BOOST_AUTO_TEST_CASE(escape_0)
 {
@@ -62,100 +44,6 @@ BOOST_AUTO_TEST_CASE(escape_2)
     escape(os, "'abc'");
     BOOST_CHECK_EQUAL(os.str(), std::string("'\\\'abc\\\''"));
 }
-
-// BOOST_AUTO_TEST_CASE(buffer_ctor_and_get)
-// {
-//     auto a = buffer(3);
-//     a.initialize<uint8_t>();
-//     BOOST_CHECK_EQUAL(a.size(), 3);
-//     BOOST_CHECK_EQUAL(a.depth(), 1);
-//     a.at<uint8_t>(0) = 0;
-//     a.at<uint8_t>(1) = 1;
-//     a.at<uint8_t>(2) = 0;
-//     BOOST_CHECK_EQUAL(a.at<uint8_t>(0), 0);
-//     BOOST_CHECK_EQUAL(a.at<uint8_t>(1), 1);
-//     BOOST_CHECK_EQUAL(a.at<uint8_t>(2), 0);
-//     BOOST_CHECK(a == a);
-//     auto b = buffer(3);
-//     b.initialize<uint8_t>();
-//     BOOST_CHECK(!(a == b));
-//     auto c = buffer(1);
-//     c.initialize<uint8_t>();
-//     BOOST_CHECK(!(a == c));
-//     auto d = buffer();
-//     BOOST_CHECK(!(a == d));
-// }
-
-// BOOST_AUTO_TEST_CASE(buffer_copy_ctor)
-// {
-//     auto a = buffer(3);
-//     a.initialize<uint8_t>();
-//     a.at<uint8_t>(1) = 1;
-//     auto b = a;
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(0), 0);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(1), 1);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(2), 0);
-//     BOOST_CHECK(a == b);
-// }
-
-// BOOST_AUTO_TEST_CASE(buffer_move_ctor)
-// {
-//     auto a = buffer(3);
-//     a.initialize<uint8_t>();
-//     a.at<uint8_t>(1) = 1;
-//     auto b = std::move(a);
-//     BOOST_CHECK_EQUAL(a.size(), 0);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(0), 0);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(1), 1);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(2), 0);
-//     BOOST_CHECK(!(a == b));
-// }
-
-
-// BOOST_AUTO_TEST_CASE(buffer_copy_assign)
-// {
-//     auto a = buffer(3);
-//     a.initialize<uint8_t>();
-//     a.at<uint8_t>(1) = 1;
-//     auto b = buffer(3);
-//     b.initialize<uint8_t>();
-//     b = a;
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(0), 0);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(1), 1);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(2), 0);
-//     BOOST_CHECK(a == b);
-// }
-
-// BOOST_AUTO_TEST_CASE(buffer_move_assign)
-// {
-//     auto a = buffer(3);
-//     a.initialize<uint8_t>();
-//     a.at<uint8_t>(1) = 1;
-//     auto b = buffer(3);
-//     b.initialize<uint8_t>();
-//     b = std::move(a);
-//     BOOST_CHECK_EQUAL(a.size(), 0);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(0), 0);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(1), 1);
-//     BOOST_CHECK_EQUAL(b.at<uint8_t>(2), 0);
-//     BOOST_CHECK(!(a == b));
-// }
-
-// BOOST_AUTO_TEST_CASE(buffer_realloc)
-// {
-//     auto a = buffer(3);
-//     a.initialize<uint8_t>();
-//     BOOST_CHECK_EQUAL(a.size(), 3);
-//     a.at<uint8_t>(0) = 1;
-//     a.at<uint8_t>(1) = 2;
-//     a.at<uint8_t>(2) = 3;
-//     a.grow<uint8_t>();
-//     BOOST_CHECK_EQUAL(a.size(), 3);
-//     BOOST_CHECK_EQUAL(a.depth(), 2);
-//     BOOST_CHECK_EQUAL(a.at<int16_t>(0), 1);
-//     BOOST_CHECK_EQUAL(a.at<int16_t>(1), 2);
-//     BOOST_CHECK_EQUAL(a.at<int16_t>(2), 3);
-// }
 
 BOOST_AUTO_TEST_CASE(tiny_string_test)
 {
