6633cab56b6b98a6241b011fb7289099761dda75
hans.dembinski@gmail.com <hdembins@mpi-hd.mpg.de>
2017-02-05 16:31:28 +0000

    switch to lightweight test

--- test/detail_test.cpp
+++ test/detail_test.cpp
@@ -4,67 +4,84 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#define BOOST_TEST_MODULE detail_test
-#include <boost/test/unit_test.hpp>
-#include <boost/test/test_tools.hpp>
+#include <boost/core/lightweight_test.hpp>
 #include <boost/histogram/detail/utility.hpp>
 #include <boost/histogram/detail/weight.hpp>
 #include <boost/histogram/detail/tiny_string.hpp>
 #include <sstream>
-#include <algorithm>
+#include <cstring>
 using namespace boost::histogram::detail;
 
-BOOST_AUTO_TEST_CASE(weight_test)
+// hotfix for cstring comparison
+namespace boost { namespace detail {
+inline void test_eq_impl( char const * expr1, char const * expr2,
+  char const * file, int line, char const * function, const char* t, const char* u )
 {
-    BOOST_CHECK(weight(0) == weight());
-    weight w(1);
-    BOOST_CHECK(w == weight(1));
-    BOOST_CHECK(w != weight());
-    BOOST_CHECK(1 == w);
-    BOOST_CHECK(w == 1);
-    BOOST_CHECK(2 != w);
-    BOOST_CHECK(w != 2);
+    if( std::strcmp(t, u) == 0 )
+    {
+        report_errors_remind();
+    }
+    else
+    {
+        BOOST_LIGHTWEIGHT_TEST_OSTREAM
+            << file << "(" << line << "): test '" << expr1 << " == " << expr2
+            << "' failed in function '" << function << "': "
+            << "'" << t << "' != '" << u << "'" << std::endl;
+        ++test_errors();
+    }
 }
+}}
 
-BOOST_AUTO_TEST_CASE(escape_0)
+int main ()
 {
-    std::ostringstream os;
-    escape(os, "abc");
-    BOOST_CHECK_EQUAL(os.str(), std::string("'abc'"));
-}
+    {
+        BOOST_TEST(weight(0) == weight());
+        weight w(1);
+        BOOST_TEST(w == weight(1));
+        BOOST_TEST(w != weight());
+        BOOST_TEST(1 == w);
+        BOOST_TEST(w == 1);
+        BOOST_TEST(2 != w);
+        BOOST_TEST(w != 2);
+    }
 
-BOOST_AUTO_TEST_CASE(escape_1)
-{
-    std::ostringstream os;
-    escape(os, "abc\n");
-    BOOST_CHECK_EQUAL(os.str(), std::string("'abc\n'"));
-}
+    {
+        std::ostringstream os;
+        escape(os, "abc");
+        BOOST_TEST_EQ(os.str(), std::string("'abc'"));
+    }
 
-BOOST_AUTO_TEST_CASE(escape_2)
-{
-    std::ostringstream os;
-    escape(os, "'abc'");
-    BOOST_CHECK_EQUAL(os.str(), std::string("'\\\'abc\\\''"));
-}
+    {
+        std::ostringstream os;
+        escape(os, "abc\n");
+        BOOST_TEST_EQ(os.str(), std::string("'abc\n'"));
+    }
 
-BOOST_AUTO_TEST_CASE(tiny_string_test)
-{
-    auto a = tiny_string();
-    BOOST_CHECK_EQUAL(a.size(), 0u);
-    BOOST_CHECK_EQUAL(a.c_str(), "");
-    auto b = tiny_string("abc");
-    BOOST_CHECK_EQUAL(b.c_str(), "abc");
-    BOOST_CHECK_EQUAL(b.size(), 3u);
-    std::ostringstream os;
-    os << b;
-    BOOST_CHECK_EQUAL(os.str(), std::string("abc"));
-    auto c = b;
-    BOOST_CHECK_EQUAL(c, b);
-    auto d = std::move(c);
-    BOOST_CHECK_EQUAL(c.c_str(), "");
-    BOOST_CHECK_EQUAL(d, b);
-    c = d;
-    BOOST_CHECK_EQUAL(c, d);
-    d = std::move(c);
-    BOOST_CHECK_EQUAL(c.c_str(), "");
+    {
+        std::ostringstream os;
+        escape(os, "'abc'");
+        BOOST_TEST_EQ(os.str(), std::string("'\\\'abc\\\''"));
+    }
+
+    {
+        auto a = tiny_string();
+        BOOST_TEST_EQ(a.size(), 0u);
+        BOOST_TEST_EQ(a.c_str(), "");
+        auto b = tiny_string("abc");
+        BOOST_TEST_EQ(b.c_str(), "abc");
+        BOOST_TEST_EQ(b.size(), 3u);
+        std::ostringstream os;
+        os << b;
+        BOOST_TEST_EQ(os.str(), std::string("abc"));
+        auto c = b;
+        BOOST_TEST_EQ(c, b);
+        auto d = std::move(c);
+        BOOST_TEST_EQ(c.c_str(), "");
+        BOOST_TEST_EQ(d, b);
+        c = d;
+        BOOST_TEST_EQ(c, d);
+        d = std::move(c);
+        BOOST_TEST_EQ(c.c_str(), "");
+    }
+    return boost::report_errors();
 }
