df6a621be21c2dc4e5af39548e1cfd4816a123eb
Hans Dembinski <hans.dembinski@gmail.com>
2017-03-23 22:25:00 +0100

    fix

--- include/boost/histogram/axis.hpp
+++ include/boost/histogram/axis.hpp
@@ -33,8 +33,8 @@ template <>
 struct bin<const std::string&>
 {
   int idx;
-  boost::string_ref value;
-  // std::string value;
+  // boost::string_ref value;
+  std::string value;
 };
 
 template <typename Value>
