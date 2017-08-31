5e365b6401da1e29f6b6295ee80cf61eba02c658
Hans Dembinski <hans.dembinski@gmail.com>
2017-09-01 00:36:17 +0200

    fixing interface

--- examples/python_example_2d.py
+++ examples/python_example_2d.py
@@ -3,9 +3,9 @@ import numpy as np
 
 h = hg.histogram(hg.axis.regular(10, -3, 3, uoflow=False),
                  hg.axis.regular(10, -3, 3, uoflow=False))
-xy = np.random.randn(2000).reshape((1000, 2))
-xy[:,1] *= 0.5
-h.fill(xy)
+x = np.random.randn(1000)
+y = 0.5 * np.random.randn(1000)
+h.fill(x, y)
 
 x = np.array(h.axis(0)) # axis instances behave like sequences
 y = np.array(h.axis(1))
