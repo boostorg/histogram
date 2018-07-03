1791c7aa68db2025da2608c935d5a7fe2392ce7b
Hans Dembinski <hans.dembinski@gmail.com>
2018-07-03 23:13:37 +0200

    fix

--- build/travis_install_boost.sh
+++ build/travis_install_boost.sh
@@ -1,6 +1,6 @@
-# must be run from build directory, can be run locally for testing
-# uses default python interpreter and patches project-config.jam as
-# workaround for a bug in the boost build scripts
+# Must be run from project directory, can be run locally for testing.
+# Uses default python interpreter and patches project-config.jam as a
+# workaround for a bug in the boost build scripts.
 BOOST_VERSION=1.65.0
 if [[ -z "${TRAVIS_BUILD_DIR}" ]]; then
   TRAVIS_BUILD_DIR=/tmp
@@ -9,7 +9,7 @@ PYVER=$(python -c 'import sys; sys.stdout.write("%i"%sys.version_info.major)')
 BOOST_DIR=${TRAVIS_BUILD_DIR}/deps/boost-${BOOST_VERSION}-py${PYVER}
 echo "Boost: ${BOOST_DIR}"
 mkdir -p ${BOOST_DIR}
-BOOSTRAP_PATCH_REGEX="s|\( *using python.*\);|\1: $(python get_python_include.py) ;|"
+BOOSTRAP_PATCH_REGEX="s|\( *using python.*\);|\1: $(python build/get_python_include.py) ;|"
 if [[ -z "$(ls -A ${BOOST_DIR})" ]]; then
   BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION//\./_}.tar.gz"
   { wget --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${BOOST_DIR}; } || exit 1
