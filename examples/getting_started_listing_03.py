e7e590e0433364870f7ed365fb82c36919259d23
Hans Dembinski <hans.dembinski@gmail.com>
2018-07-16 23:19:16 +0200

    fix

--- examples/getting_started_listing_03.py
+++ examples/getting_started_listing_03.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import sys
 import os
 sys.path.append(os.getcwd())
@@ -40,7 +41,7 @@ try:
     plt.savefig("example_2d_python.png")
 except ImportError:
     # ok, no matplotlib, then just print the full count matrix
-    print count_matrix
+    print(count_matrix)
 
     # output of the print looks something like this, the two right-most rows
     # and two down-most columns represent under-/overflow bins
