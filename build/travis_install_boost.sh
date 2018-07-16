27ddca31c2e5f36b73b403ea831dcc8880a841c7
Hans Dembinski <hans.dembinski@gmail.com>
2018-07-16 22:01:23 +0200

    fix

--- build/travis_install_boost.sh
+++ build/travis_install_boost.sh
@@ -9,12 +9,15 @@ PYVER=$(python -c 'import sys; sys.stdout.write("%i"%sys.version_info.major)')
 BOOST_DIR=${TRAVIS_BUILD_DIR}/deps/boost-${BOOST_VERSION}-py${PYVER}
 echo "Boost: ${BOOST_DIR}"
 mkdir -p ${BOOST_DIR}
-BOOSTRAP_PATCH_REGEX="s|\( *using python.*\);|\1: $(python build/get_python_include.py) ;|"
+BOOTSTRAP_PATCH_REGEX="s|\( *using python.*\);|\1: $(python build/get_python_include.py) ;|"
+echo "Bootstrap patch line: ${BOOTSTRAP_PATCH_REGEX}"
 if [[ -z "$(ls -A ${BOOST_DIR})" ]]; then
   BOOST_URL="http://sourceforge.net/projects/boost/files/boost/${BOOST_VERSION}/boost_${BOOST_VERSION//\./_}.tar.gz"
   { wget --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${BOOST_DIR}; } || exit 1
   (cd ${BOOST_DIR} && ./bootstrap.sh > /dev/null && \
-   sed -i "${BOOSTRAP_PATCH_REGEX}" project-config.jam && \
+   cat project-config.jam && \
+   sed -i "${BOOTSTRAP_PATCH_REGEX}" project-config.jam && \
+   cat project-config.jam && \
    ./b2 install --prefix=${BOOST_DIR} --with-serialization --with-iostreams --with-python | grep -v -e common\.copy -e common\.mkdir) || exit 1
 fi
 ls ${BOOST_DIR}/lib | grep libboost
