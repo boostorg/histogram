6f94a05fd244b53bae10c4bb87e498128583cfde
Hans Dembinski <hans.dembinski@gmail.com>
2018-09-26 11:42:47 +0200

    WIP, added tests of failing behavior

---
+++ test/histogram_dynamic_fill_one_dimensional_vector_fail.cpp
@@ -0,0 +1,14 @@
+// Copyright 2018 Hans Dembinski
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/histogram.hpp>
+#include <vector>
+
+using namespace boost::histogram;
+int main() {
+  auto a = make_dynamic_histogram(axis::integer<>(0, 2));
+  a(std::vector<int>(1));
+}
