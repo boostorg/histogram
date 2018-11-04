cf8c7a1168eb2cf5ae99a2dd34784fb91b3d2d46
Hans Dembinski <hans.dembinski@gmail.com>
2018-11-04 23:51:26 +0100

    avoid warnings on travis with gcc-5

--- include/boost/histogram/detail/meta.hpp
+++ include/boost/histogram/detail/meta.hpp
@@ -7,13 +7,14 @@
 #ifndef BOOST_HISTOGRAM_DETAIL_META_HPP
 #define BOOST_HISTOGRAM_DETAIL_META_HPP
 
-#if defined(BOOST_GCC)
+#include <boost/config/workaround.hpp>
+#if BOOST_WORKAROUND(BOOST_GCC, >= 60000)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wnoexcept-type"
 #endif
 #include <boost/callable_traits/args.hpp>
 #include <boost/callable_traits/return_type.hpp>
-#if defined(BOOST_GCC)
+#if BOOST_WORKAROUND(BOOST_GCC, >= 60000)
 #pragma GCC diagnostic pop
 #endif
 #include <boost/histogram/histogram_fwd.hpp>
