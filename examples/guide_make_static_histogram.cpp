c3013260f1f5c6d8e1570e8ce0aa81371029fde0
Hans Dembinski <hans.dembinski@gmail.com>
2018-11-13 15:12:41 +0100

    added license

--- examples/guide_make_static_histogram.cpp
+++ examples/guide_make_static_histogram.cpp
@@ -1,3 +1,9 @@
+// Copyright 2015-2018 Hans Dembinski
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 //[ guide_make_static_histogram
 
 #include <boost/histogram.hpp>
