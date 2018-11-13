bf7170c9a18394df140213ba32732d25c0e40857
Hans Dembinski <hans.dembinski@gmail.com>
2018-11-13 16:16:14 +0100

    fix ci

--- test/algorithm_sum_test.cpp
+++ test/algorithm_sum_test.cpp
@@ -24,18 +24,15 @@ void run_tests() {
   auto h2 = make_s(Tag(), std::vector<double>(), ax, ax);
   for (unsigned i = 0; i < 100; ++i)
     for (unsigned j = 0; j < 100; ++j) h2(i, j);
-
-  BOOST_TEST_EQ(sum(h2), 100000);
+  BOOST_TEST_EQ(sum(h2), 10000);
 
   auto h3 = make_s(Tag(), std::array<int, 102>(), ax);
   for (unsigned i = 0; i < 100; ++i) h3(i);
-
   BOOST_TEST_EQ(sum(h3), 100);
 
   auto h4 = make_s(Tag(), std::unordered_map<std::size_t, int>(), ax);
-  for (unsigned i = 0; i < 100; ++i) h3(i);
-
-  BOOST_TEST_EQ(sum(h3), 100);
+  for (unsigned i = 0; i < 100; ++i) h4(i);
+  BOOST_TEST_EQ(sum(h4), 100);
 }
 
 int main() {
