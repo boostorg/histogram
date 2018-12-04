74cc4aab8a1597f838c3b6a7b412af8e8e9eeac9
Hans Dembinski <hans.dembinski@gmail.com>
2018-12-04 13:32:04 +0100

    faster indexing when iterating over all bins

--- include/boost/histogram/indexed.hpp
+++ include/boost/histogram/indexed.hpp
@@ -26,7 +26,7 @@ class BOOST_HISTOGRAM_NODISCARD indexed_range {
   using axes_type = typename Histogram::axes_type;
   using value_type = typename Histogram::value_type;
   using value_iterator = typename Histogram::const_iterator;
-  using axis_data_type = detail::axes_buffer<axes_type, std::pair<std::size_t, int>>;
+  using axis_data_type = detail::axes_buffer<axes_type, std::pair<int, int>>;
   using index_type = detail::axes_buffer<axes_type, int>;
 
 public:
@@ -58,7 +58,7 @@ public:
       return *iter_;
     }
 
-    decltype(auto) operator->() const noexcept {
+    value_iterator operator->() const noexcept {
       return iter_;
     }
 
@@ -80,16 +80,36 @@ public:
     }
 
     void iter_over_all() noexcept {
-      auto k = ++iter_ - parent_.hist_.begin();
-      auto s = parent_.axis_data_.end();
-      auto i = parent_.index_.end();
-      while (s != parent_.axis_data_.begin()) {
-        --s;
-        --i;
-        *i = k / s->first;
-        k %= s->first;
-        if (*i == s->second) *i = -1;
+      auto s = parent_.axis_data_.begin();
+      auto i = parent_.index_.begin();
+      if (*i >= 0) {
+        ++*i;
+        if (*i == s->second)
+          *i = -1;
+      } else {
+        *i = s->first;
       }
+      while (*i == s->first && i < (parent_.index_.end() - 1)) {
+        *i = 0;
+        ++i;
+        ++s;
+        if (*i >= 0) {
+          ++*i;
+          if (*i == s->second)
+            *i = -1;
+        } else {
+          *i = s->first;
+        }
+      }
+
+      std::size_t k = 0, stride = 1;
+      s = parent_.axis_data_.begin();
+      for (auto& x : parent_.index_) {
+        k += (x >= 0 ? x : s->second) * stride;
+        stride *= s->first;
+        ++s;
+      }
+      iter_ = parent_.hist_.begin() + k;
     }
 
     void iter_over_inner() noexcept {
@@ -137,14 +157,11 @@ public:
       , axis_data_(h.rank())
       , index_(h.rank(), 0) {
     auto it = axis_data_.begin();
-    std::size_t s = 1;
     h.for_each_axis([&](const auto& a) {
+      it->first = axis::traits::extend(a);
       if (include_extra_bins_) {
-        it->first = s;
-        s *= axis::traits::extend(a);
         it->second = axis::traits::underflow_index(a);
       } else {
-        it->first = axis::traits::extend(a);
         it->second = a.size();
       }
       ++it;
