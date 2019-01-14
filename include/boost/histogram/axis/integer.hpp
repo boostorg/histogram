3ed77e7287d91e774c4ba61c86a858325c584291
Hans Dembinski <hans.dembinski@gmail.com>
2019-01-14 22:00:33 +0100

    nicer way of injecting optional interface

--- include/boost/histogram/axis/integer.hpp
+++ include/boost/histogram/axis/integer.hpp
@@ -23,9 +23,19 @@ namespace boost {
 namespace histogram {
 namespace axis {
 
+template <class, class, bool>
+class optional_integer_mixin {};
+
+/** Axis for an interval of integer values with unit steps.
+ *
+ * Binning is a O(1) operation. This axis operates
+ * faster than a regular axis.
+ */
 template <class IntType, class MetaData, option Options>
-class integer_base : public base<MetaData, Options>,
-                     public iterator_mixin<integer<IntType, MetaData, Options>> {
+class integer : public base<MetaData, Options>,
+                public iterator_mixin<integer<IntType, MetaData, Options>>,
+                public optional_integer_mixin<integer<IntType, MetaData, Options>,
+                                              IntType, test(Options, option::growth)> {
   static_assert(!test(Options, option::circular) || !test(Options, option::underflow),
                 "circular axis cannot have underflow");
   static_assert(!std::is_integral<IntType>::value || std::is_same<IntType, int>::value,
@@ -36,7 +46,7 @@ class integer_base : public base<MetaData, Options>,
   using index_type = std::conditional_t<std::is_integral<value_type>::value, int, double>;
 
 public:
-  integer_base() = default;
+  integer() = default;
 
   /** Construct over semi-open integer interval [start, stop).
    *
@@ -45,13 +55,13 @@ public:
    * \param metadata description of the axis.
    * \param options  extra bin options.
    */
-  integer_base(value_type start, value_type stop, metadata_type m = {})
+  integer(value_type start, value_type stop, metadata_type m = {})
       : base_type(static_cast<unsigned>(stop - start > 0 ? stop - start : 0),
                   std::move(m))
       , min_(start) {}
 
   /// Constructor used by algorithm::reduce to shrink and rebin.
-  integer_base(const integer_base& src, int begin, int end, unsigned merge)
+  integer(const integer& src, int begin, int end, unsigned merge)
       : base_type(end - begin, src.metadata()), min_(src.min_ + begin) {
     if (merge > 1)
       BOOST_THROW_EXCEPTION(std::invalid_argument("cannot merge bins for integer axis"));
@@ -77,11 +87,11 @@ public:
     return subscript_impl(std::is_same<index_type, double>(), idx);
   }
 
-  bool operator==(const integer_base& o) const noexcept {
+  bool operator==(const integer& o) const noexcept {
     return base_type::operator==(o) && min_ == o.min_;
   }
 
-  bool operator!=(const integer_base& o) const noexcept { return !operator==(o); }
+  bool operator!=(const integer& o) const noexcept { return !operator==(o); }
 
   template <class Archive>
   void serialize(Archive&, unsigned);
@@ -110,7 +120,7 @@ protected:
   }
 
   decltype(auto) subscript_impl(std::true_type, int idx) const noexcept {
-    return interval_view<integer_base>(*this, idx);
+    return interval_view<integer>(*this, idx);
   }
 
   decltype(auto) subscript_impl(std::false_type, int idx) const noexcept {
@@ -118,30 +128,16 @@ protected:
   }
 
   int min_ = 0;
+
+  template <class, class, bool>
+  friend class optional_integer_mixin;
 };
 
-/** Axis for an interval of integer values with unit steps.
- *
- * Binning is a O(1) operation. This axis operates
- * faster than a regular axis.
- */
-template <class IntType, class MetaData, option Options>
-class integer : public integer_base<IntType, MetaData, Options> {
-  using base_type = integer_base<IntType, MetaData, Options>;
+template <class Derived, class V>
+class optional_integer_mixin<Derived, V, true> {
+  using value_type = V;
 
 public:
-  using base_type::base_type;
-};
-
-template <class IntType, class MetaData>
-class integer<IntType, MetaData, option::growth>
-    : public integer_base<IntType, MetaData, option::growth> {
-  using base_type = integer_base<IntType, MetaData, option::growth>;
-  using value_type = IntType;
-
-public:
-  using base_type::base_type;
-
   /// Returns index and shift (if axis has grown) for the passed argument.
   auto update(value_type x) {
     return update_impl(std::is_floating_point<value_type>(), x);
@@ -149,15 +145,15 @@ public:
 
 private:
   auto update_impl(std::false_type, int x) noexcept {
-    const auto i = x - base_type::min_;
+    const auto i = x - static_cast<Derived*>(this)->min_;
     if (i >= 0) {
-      if (i < base_type::size()) return std::make_pair(i, 0);
-      const auto n = i - base_type::size() + 1;
-      base_type::grow(n);
+      if (i < static_cast<Derived*>(this)->size()) return std::make_pair(i, 0);
+      const auto n = i - static_cast<Derived*>(this)->size() + 1;
+      static_cast<Derived*>(this)->grow(n);
       return std::make_pair(i, -n);
     }
-    base_type::min_ += i;
-    base_type::grow(-i);
+    static_cast<Derived*>(this)->min_ += i;
+    static_cast<Derived*>(this)->grow(-i);
     return std::make_pair(0, -i);
   }
 
