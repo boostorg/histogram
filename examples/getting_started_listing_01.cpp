58bc54f52ea821e0c565644cc8670619c226887e
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-03 12:44:26 +0100

    improved examples

--- examples/getting_started_listing_01.cpp
+++ examples/getting_started_listing_01.cpp
@@ -8,7 +8,7 @@
 
 #include <algorithm>           // std::for_each
 #include <boost/format.hpp>    // only needed for printing
-#include <boost/histogram.hpp> // make_histogram, weight, indexed
+#include <boost/histogram.hpp> // make_histogram, regular, weight, indexed
 #include <cassert>             // assert
 #include <functional>          // std::ref
 #include <sstream>             // std::ostringstream, std::cout, std::flush
@@ -17,52 +17,52 @@ int main() {
   using namespace boost::histogram; // strip the boost::histogram prefix
 
   /*
-    Create a 1d-histogram with an axis that has 6 equidistant
-    bins on the real line from -1.0 to 2.0, and label it as "x".
+    Create a 1d-histogram with a regular axis that has 6 equidistant bins on the real line
+    from -1.0 to 2.0, and label it as "x". A family of overloaded factory functions called
+    `make_histogram` makes creating histograms easy.
+
+    A regular axis is a sequence of semi-open bins. Extra under- and overflow bins extend
+    the axis by default (this can be turned off).
+
+    index    :      -1  |  0  |  1  |  2  |  3  |  4  |  5  |  6
+    bin edges:  -inf  -1.0  -0.5   0.0   0.5   1.0   1.5   2.0   inf
   */
   auto h = make_histogram(axis::regular<>(6, -1.0, 2.0, "x"));
 
   /*
-    Fill histogram with data, typically this happens in a loop.
+    Let's fill a histogram with data, typically this happens in a loop.
 
-    A regular axis is a sequence of semi-open bins. Extra under- and
-    overflow bins extend the axis in the default configuration.
-    index    :      -1     0     1    2    3    4    5    6
-    bin edges:  -inf  -1.0  -0.5  0.0  0.5  1.0  1.5  2.0  inf
-
-    STL algorithms are supported. std::for_each is very convenient
-    to fill a histogram from an iterator range. Make sure to
-    use std::ref in the call, otherwise it will fill a copy of
-    the histogram and return it, which is less efficient.
+    STL algorithms are supported. std::for_each is very convenient to fill a histogram
+    from an iterator range. Use std::ref in the call, if you don't want std::for_each to
+    make a copy of your histogram.
   */
-
   auto data = {-0.5, 1.1, 0.3, 1.7};
   std::for_each(data.begin(), data.end(), std::ref(h));
-  h(-1.5); // put in underflow bin -1
-  h(-1.0); // put in bin 0, bin interval is semi-open
-  h(2.0);  // put in overflow bin 6, bin interval is semi-open
-  h(20.0); // put in overflow bin 6
+  h(-1.5); // is placed in underflow bin -1
+  h(-1.0); // is placed in bin 0, bin interval is semi-open
+  h(2.0);  // is placed in overflow bin 6, bin interval is semi-open
+  h(20.0); // is placed in overflow bin 6
 
   /*
     This does a weighted fill using the `weight` function as an additional argument. It
     may appear at the beginning or end of the argument list. C++ doesn't have keyword
-    arguments, this is the next-best thing.
+    arguments like Python, this is the next-best thing.
   */
   h(0.1, weight(1.0));
 
   /*
-    Iterate over bins with the `indexed` range adaptor to obtain the current bin index and
-    the bin value via an accessor class. By default, under- and overflow bins are skipped.
-    Passing `coverage::all` as the optional second argument iterates over all bins.
+    Iterate over bins with the `indexed` range generator, which provides a special
+    accessor object, that can be used to obtain the current bin index, and the current bin
+    value by dereferncing (it acts like a pointer to the value). Using `indexed` is
+    convenient and gives you better performance than looping over the histogram cells with
+    hand-written for loops. By default, under- and overflow bins are skipped. Passing
+    `coverage::all` as the optional second argument iterates over all bins.
 
-    - Access the value with the dereference operator. The proxy acts like a pointer to it.
-    - Access the current index with `index(d)` of the accessor, passing the dimension d.
-    - Access the corresponding bin interval view with `bin(d)`. Use a compile-time number
-      like 1_c instead of a normal number like 1, if possible, to make this call more
-      efficent. The return type depends on the axis type (see the axis reference for
-      details), usually a class that represents a semi-open interval, whose edges can be
-      accessed with methods `lower()` and `upper()`.
-    - Iteration order is implementation defined, `indexed` uses the most efficient one.
+    - Access the value with the dereference operator.
+    - Access the current index with `index(d)` method of the accessor, with dimension d.
+    - Access the corresponding bin interval view with `bin(d)`. The return type depends on
+      the axis type (see the axis reference for details), usually a class that represents
+      a semi-open interval. Edges can be accessed with methods `lower()` and `upper()`.
   */
   using namespace boost::histogram::literals; // import compile-time numbers 0_c, 1_c, ...
 
--- examples/guide_custom_storage.cpp
+++ examples/guide_custom_storage.cpp
@@ -6,34 +6,41 @@
 
 //[ guide_custom_storage
 
+#include <algorithm> // std::for_each
 #include <array>
 #include <boost/histogram.hpp>
+#include <boost/histogram/algorithm/sum.hpp>
+#include <functional> // std::ref
 #include <unordered_map>
 #include <vector>
 
-namespace bh = boost::histogram;
-
 int main() {
-  // create static histogram with vector<int> as counter storage,
-  // you can use other arithmetic types as counters, e.g. double
-  auto h1 =
-      bh::make_histogram_with(std::vector<int>(), bh::axis::regular<>(10, 0.0, 1.0));
+  using namespace boost::histogram;
+  const auto axis = axis::regular<>(10, 0.0, 1.0);
 
-  // create static histogram with array<int, N> as counter storage
-  // which is allocated completely on the stack (this is very fast)
-  auto h2 =
-      bh::make_histogram_with(std::array<int, 12>(), bh::axis::regular<>(10, 0.0, 1.0));
-  // N may be larger than the actual number of bins used; an exception
-  // is raised if N is too small to hold all bins
+  auto data = {0.1, 0.3, 0.2, 0.7};
 
-  // create static histogram with unordered_map as counter storage;
-  // this generates a sparse histogram where only memory is allocated
-  // for bins that are non-zero
-  auto h3 = bh::make_histogram_with(std::unordered_map<std::size_t, int>(),
-                                    bh::axis::regular<>(10, 0.0, 1.0));
-  // this sounds like a good idea for high-dimensional histograms,
-  // but maps come with a signficant memory and run-time overhead; the
-  // default_storage usually performs better in high dimensions
+  // Create static histogram with vector<int> as counter storage, you can use other
+  // arithmetic types as counters, e.g. double.
+  auto h1 = make_histogram_with(std::vector<int>(), axis);
+  std::for_each(data.begin(), data.end(), std::ref(h1));
+  assert(algorithm::sum(h1) == 4);
+
+  // Create static histogram with array<int, N> as counter storage which is allocated
+  // completely on the stack (this is very fast). N may be larger than the actual number
+  // of bins used; an exception is raised if N is too small to hold all bins.
+  auto h2 = make_histogram_with(std::array<int, 12>(), axis);
+  std::for_each(data.begin(), data.end(), std::ref(h2));
+  assert(algorithm::sum(h2) == 4);
+
+  // Create static histogram with unordered_map as counter storage; this generates a
+  // sparse histogram where only memory is allocated for bins that are non-zero. This
+  // sounds like a good idea for high-dimensional histograms, but maps come with a
+  // memory and run-time overhead. The default_storage usually performs better
+  // in high dimensions.
+  auto h3 = make_histogram_with(std::unordered_map<std::size_t, int>(), axis);
+  std::for_each(data.begin(), data.end(), std::ref(h3));
+  assert(algorithm::sum(h3) == 4);
 }
 
 //]
