e6abb0d885c6dbd65090a74b647f42f996b9b62a
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-10 15:15:38 +0100

    now the example rely relies on run-time info

--- examples/guide_make_dynamic_histogram.cpp
+++ examples/guide_make_dynamic_histogram.cpp
@@ -7,28 +7,36 @@
 //[ guide_make_dynamic_histogram
 
 #include <boost/histogram.hpp>
+#include <boost/lexical_cast.hpp> // convert strings to numbers
 #include <cassert>
 #include <vector>
 
 namespace bh = boost::histogram;
 
-int main() {
-  // create vector of three regular axes
+int main(int argc, char** argv) {
+  // read axis config from command-line: [nbins start stop] ...
+  // and create vector of regular axes, the number of axis is not known at compile-time
+  assert(argc - 1 % 3 == 0);
   auto v1 = std::vector<bh::axis::regular<>>();
-  v1.emplace_back(10, 0.0, 1.0);
-  v1.emplace_back(20, 0.0, 2.0);
-  v1.emplace_back(30, 0.0, 3.0);
+  for (int iarg = 1; iarg < argc;) {
+    const auto bins = boost::lexical_cast<unsigned>(argv[iarg++]);
+    const auto start = boost::lexical_cast<double>(argv[iarg++]);
+    const auto stop = boost::lexical_cast<double>(argv[iarg++]);
+    v1.emplace_back(bins, start, stop);
+  }
 
   // create dynamic histogram from iterator range
+  // (copying or moving the vector also works, move is shown below)
   auto h1 = bh::make_histogram(v1.begin(), v1.end());
-  assert(h1.rank() == 3);
+  assert(h1.rank() == v1.size());
 
-  // create second vector of axis::any, a polymorphic axis type
+  // create second vector of axis::variant, a polymorphic axis type that can hold concrete
+  // axis types, the types and the number of axis can now vary at run-time
   auto v2 = std::vector<bh::axis::variant<bh::axis::regular<>, bh::axis::integer<>>>();
   v2.emplace_back(bh::axis::regular<>(100, -1.0, 1.0));
   v2.emplace_back(bh::axis::integer<>(1, 7));
 
-  // create dynamic histogram by moving the vector (avoids copies)
+  // create dynamic histogram by moving the vector
   auto h2 = bh::make_histogram(std::move(v2));
   assert(h2.rank() == 2);
 }
