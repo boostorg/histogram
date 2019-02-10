38c2bd45884f9735656c6a84964db3ddeca523fd
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-10 22:35:41 +0100

    fix [ci skip]

--- doc/push_docs.sh
+++ doc/push_docs.sh
@@ -1,6 +1,1 @@
-git co gh-pages
-git merge develop -m automerge
-rm -rf html
-b2
-git ci html -m update
-git co develop
+git co gh-pages && git merge develop -m automerge && rm -rf html && b2 && git ci html -m update && git push && git co develop
