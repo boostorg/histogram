7ba58143534e9858864924bfc87fe349f4e94cff
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-14 20:22:39 +0100

    fix coverage

--- tools/cov.sh
+++ tools/cov.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-# must be executed in project folder
-mkdir -p test/cov
-mkdir -p examples/cov
-cp -r ../../bin.v2/libs/histogram/test/* test/cov
-cp -r ../../bin.v2/libs/histogram/examples/* examples/cov
-curl -s https://codecov.io/bash | bash -s -
+# must be executed in project root folder
+lcov --rc lcov_branch_coverage=1 --base-directory `pwd`/test --directory `pwd`/../../bin.v2/libs/histogram/test --capture --output-file test.info
+lcov --rc lcov_branch_coverage=1 --base-directory `pwd`/examples --directory `pwd`/../../bin.v2/libs/histogram/examples --capture --output-file examples.info
+
+# merge files
+lcov --rc lcov_branch_coverage=1 -a test.info -a examples.info -o all.info
+
+# remove uninteresting entries
+lcov --rc lcov_branch_coverage=1 --extract all.info "*/boost/histogram/*" --output-file coverage.info
+
+# upload
+curl -s https://codecov.io/bash | bash -s - -f coverage.info -X gcov
