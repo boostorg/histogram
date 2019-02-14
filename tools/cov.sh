ec88335cfba0b48b947bd7f07c5217c04f0327cd
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-14 12:28:39 +0100

    fix script maybe

--- tools/cov.sh
+++ tools/cov.sh
@@ -1,4 +1,4 @@
 # must be executed in test folder
 mkdir -p cov
 cp -r ../../../bin.v2/libs/histogram/test/* cov
-bash <(curl -s https://codecov.io/bash) -f '!*.cpp'
+bash <(curl -s https://codecov.io/bash)
