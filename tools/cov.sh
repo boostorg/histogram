dfd8b86ab05abf8f1c0bf57b55354ce320f0d1cd
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-14 20:58:00 +0100

    use newer lcov

--- tools/cov.sh
+++ tools/cov.sh
@@ -5,14 +5,18 @@ if [ -z $GCOV ]; then
 fi
 LCOV_OPTS="--gcov-tool=${GCOV} --rc lcov_branch_coverage=1"
 
-lcov $LCOV_OPTS --base-directory `pwd`/test --directory `pwd`/../../bin.v2/libs/histogram/test --capture --output-file test.info
-lcov $LCOV_OPTS --base-directory `pwd`/examples --directory `pwd`/../../bin.v2/libs/histogram/examples --capture --output-file examples.info
+wget https://github.com/linux-test-project/lcov/releases/download/v1.13/lcov-1.13.tar.gz
+tar xzf lcov-1.13.tar.gz
+LCOV="`pwd`/lcov-1.13/bin/lcov ${LCOV_OPTS}"
+
+$LCOV --base-directory `pwd`/test --directory `pwd`/../../bin.v2/libs/histogram/test --capture --output-file test.info
+$LCOV --base-directory `pwd`/examples --directory `pwd`/../../bin.v2/libs/histogram/examples --capture --output-file examples.info
 
 # merge files
-lcov $LCOV_OPTS -a test.info -a examples.info -o all.info
+$LCOV -a test.info -a examples.info -o all.info
 
 # remove uninteresting entries
-lcov $LCOV_OPTS --extract all.info "*/boost/histogram/*" --output-file coverage.info
+$LCOV --extract all.info "*/boost/histogram/*" --output-file coverage.info
 
 # upload
 curl -s https://codecov.io/bash | bash -s - -f coverage.info -X gcov -x $GCOV
