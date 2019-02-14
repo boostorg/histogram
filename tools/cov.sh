b173ff8d01986f31696aa1d8e87e4fc17e27a0c1
Hans Dembinski <hans.dembinski@gmail.com>
2019-02-14 20:39:47 +0100

    specify gcov

--- tools/cov.sh
+++ tools/cov.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # must be executed in project root folder
-lcov --rc lcov_branch_coverage=1 --base-directory `pwd`/test --directory `pwd`/../../bin.v2/libs/histogram/test --capture --output-file test.info
-lcov --rc lcov_branch_coverage=1 --base-directory `pwd`/examples --directory `pwd`/../../bin.v2/libs/histogram/examples --capture --output-file examples.info
+if [ -z $GCOV ]; then
+  GCOV=gcov
+fi
+LCOV_OPTS="--gcov-tool=${GCOV} --rc lcov_branch_coverage=1"
+
+lcov $LCOV_OPTS --base-directory `pwd`/test --directory `pwd`/../../bin.v2/libs/histogram/test --capture --output-file test.info
+lcov $LCOV_OPTS --base-directory `pwd`/examples --directory `pwd`/../../bin.v2/libs/histogram/examples --capture --output-file examples.info
 
 # merge files
-lcov --rc lcov_branch_coverage=1 -a test.info -a examples.info -o all.info
+lcov $LCOV_OPTS -a test.info -a examples.info -o all.info
 
 # remove uninteresting entries
-lcov --rc lcov_branch_coverage=1 --extract all.info "*/boost/histogram/*" --output-file coverage.info
+lcov $LCOV_OPTS --extract all.info "*/boost/histogram/*" --output-file coverage.info
 
 # upload
-curl -s https://codecov.io/bash | bash -s - -f coverage.info -X gcov
+curl -s https://codecov.io/bash | bash -s - -f coverage.info -X gcov -x $GCOV
