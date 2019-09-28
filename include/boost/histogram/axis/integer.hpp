a73bbc13d42f331dde996efeb220f4a233188afd
Hans Dembinski <hans.dembinski@gmail.com>
2019-09-29 00:18:58 +0200

    integer<int, ...> cannot use *flow when circular or growing

--- include/boost/histogram/axis/integer.hpp
+++ include/boost/histogram/axis/integer.hpp
@@ -50,10 +50,14 @@ class integer : public iterator_mixin<integer<Value, MetaData, Options>> {
   using options_type =
       detail::replace_default<Options, decltype(option::underflow | option::overflow)>;
 
-  static_assert(!options_type::test(option::circular) ||
-                    std::is_floating_point<value_type>::value ||
-                    !options_type::test(option::overflow),
-                "integer axis with integral type cannot have overflow");
+  static_assert(!std::is_floating_point<value_type>::value &&
+                        (options_type::test(option::circular) ||
+                         options_type::test(option::growth))
+                    ? !options_type::test(option::overflow) &&
+                          !options_type::test(option::underflow)
+                    : true,
+                "circular or growing integer axis with integral type cannot have "
+                "underflow or overflow");
 
 public:
   constexpr integer() = default;
