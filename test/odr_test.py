2d5731b92918e39810112de26b2ee4cb343783fd
Hans Dembinski <hans.dembinski@gmail.com>
2019-10-12 12:01:41 +0200

    copyright and fixes

--- test/odr_test.py
+++ test/odr_test.py
@@ -1,3 +1,9 @@
+# Copyright 2019 Hans Dembinski, Henry Schreiner
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt
+# or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 import os
 import sys
 import re
@@ -8,9 +14,9 @@ all_headers = set()
 include_path = os.path.join(this_path, "..", "include")
 for root, dirs, files in os.walk(include_path):
     for fn in files:
-        fn = os.path.join(root,  fn)
+        fn = os.path.join(root, fn)
         assert fn.startswith(include_path)
-        fn = fn[len(include_path) :]
+        fn = fn[len(include_path) + 1 :]
         all_headers.add(fn)
 
 
@@ -24,12 +30,12 @@ def get_headers(filename):
 
 included_headers = set()
 unread_headers = set()
-for hdr in get_headers(os.path.join(this_path, "/odr_test.cpp")):
+for hdr in get_headers(os.path.join(this_path, "odr_test.cpp")):
     unread_headers.add(hdr)
 
 while unread_headers:
     included_headers.update(unread_headers)
-    for hdr in tuple(unread_headers):
+    for hdr in tuple(unread_headers):  # copy needed because unread_headers is modified
         unread_headers.remove(hdr)
         for hdr2 in get_headers(os.path.join(include_path, hdr)):
             if hdr2 not in included_headers:
