48d411fbdd42fd35be0fb575d23ed573100f5b76
Hans Dembinski <hans.dembinski@gmail.com>
2019-11-04 21:07:09 +0100

    docu for weight_type

--- include/boost/histogram/weight.hpp
+++ include/boost/histogram/weight.hpp
@@ -12,9 +12,18 @@
 namespace boost {
 namespace histogram {
 
+/** Weight holder and type envelope.
+
+  You should not construct these directly, use the weight() helper function.
+
+  @tparam Underlying arithmetic type.
+*/
 template <class T>
 struct weight_type {
+  /// Access underlying value.
   T value;
+
+  /// Allow implicit conversions of types when the underlying value type allows them.
   template <class U>
   operator weight_type<U>() const {
     return weight_type<U>{static_cast<U>(value)};
