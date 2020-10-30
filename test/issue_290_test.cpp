ab59a5eef0221398fff50d4f7e4431bc9c11f2a5
Hans Dembinski <hans.dembinski@gmail.com>
2020-10-30 22:58:28 +0100

    avoid type_traits dep

--- test/issue_290_test.cpp
+++ test/issue_290_test.cpp
@@ -4,11 +4,15 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/type_traits.hpp>
+// - bug only appears on cxxstd=17 or higher and only in gcc
+// - reported in issue: https://github.com/boostorg/histogram/issues/290
+// - originally caused by rank struct in boost/type_traits/rank.hpp,
+//   which we emulate here to avoid the dependency on boost.type_traits
+
+// Original: #include <boost/type_traits/rank.hpp>
+template <class T>
+struct rank;
 
 #include <boost/histogram/axis/traits.hpp>
-
-// bug only appears on cxxstd=17 or higher and only in gcc
-// reported in issue: https://github.com/boostorg/histogram/issues/290
 
 int main() { return 0; }
