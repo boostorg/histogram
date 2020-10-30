71a27a190ee58a8117be0992c78a1f03aaa78937
Hans Dembinski <hans.dembinski@gmail.com>
2020-10-30 12:36:36 +0100

    wip

---
+++ test/rank_bug_test.cpp
@@ -0,0 +1,16 @@
+// Copyright 2020 Hans Dembinski
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/type_traits.hpp> // must be first
+#include <boost/histogram.hpp>
+#include "throw_exception.hpp"
+
+// bug only appears on cxxstd=17 or higher and only in gcc
+// reported in issue: https://github.com/boostorg/histogram/issues/290
+
+int main() {
+  return 0;
+}
