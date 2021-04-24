debfad052faccfa73285474498bc036afd930d36
Hans Dembinski <hans.dembinski@gmail.com>
2021-04-24 17:14:07 +0200

    wip

--- include/boost/histogram/indexed.hpp
+++ include/boost/histogram/indexed.hpp
@@ -11,15 +11,27 @@
 #include <boost/config.hpp> // BOOST_ATTRIBUTE_NODISCARD
 #include <boost/histogram/axis/traits.hpp>
 #include <boost/histogram/detail/axes.hpp>
+#include <boost/histogram/detail/debug.hpp>
+#include <boost/histogram/detail/detect.hpp>
 #include <boost/histogram/detail/iterator_adaptor.hpp>
 #include <boost/histogram/detail/operators.hpp>
 #include <boost/histogram/fwd.hpp>
+#include <boost/histogram/unsafe_access.hpp>
 #include <iterator>
 #include <type_traits>
-#include <utility>
+#include <utility> // std::get
 
 namespace boost {
 namespace histogram {
+
+namespace detail {
+using std::get;
+
+template <std::size_t I, class T>
+auto get(T&& t) -> decltype(t[0]) {
+  return t[0];
+}
+} // namespace detail
 
 /** Coverage mode of the indexed range generator.
 
@@ -304,31 +316,42 @@ public:
   };
 
   indexed_range(histogram_type& hist, coverage cov)
+      : indexed_range(hist, make_range(hist, cov)) {
+    DEBUG("foo");
+  }
+
+  template <class Iterable, class = detail::requires_iterable<Iterable>>
+  indexed_range(histogram_type& hist, Iterable&& range)
       : begin_(hist.begin(), hist), end_(hist.end(), hist) {
-    begin_.indices_.hist_->for_each_axis([ca = begin_.indices_.begin(), cov,
+    auto r_begin = std::begin(range);
+    assert(std::distance(r_begin, std::end(range)) == hist.rank());
+
+    begin_.indices_.hist_->for_each_axis([ca = begin_.indices_.begin(), r_begin,
                                           stride = std::size_t{1},
                                           this](const auto& a) mutable {
-      using opt = axis::traits::get_options<std::decay_t<decltype(a)>>;
-      constexpr axis::index_type under = opt::test(axis::option::underflow);
-      constexpr axis::index_type over = opt::test(axis::option::overflow);
       const auto size = a.size();
 
-      // -1 if underflow and cover all, else 0
-      ca->begin = cov == coverage::all ? -under : 0;
-      // size + 1 if overflow and cover all, else size
-      ca->end = cov == coverage::all ? size + over : size;
+      using opt = axis::traits::get_options<std::decay_t<decltype(a)>>;
+      constexpr axis::index_type start = opt::test(axis::option::underflow) ? -1 : 0;
+      const auto stop = size + (opt::test(axis::option::overflow) ? 1 : 0);
+
+      ca->begin = std::max(start, detail::get<0>(*r_begin));
+      ca->end = std::min(stop, detail::get<1>(*r_begin));
       ca->idx = ca->begin;
 
-      // if axis has *flow and coverage::all OR axis has no *flow:
-      //   begin + under == 0, size + over - end == 0
-      // if axis has *flow and coverage::inner:
-      //   begin + under == 1, size + over - end == 1
-      ca->begin_skip = static_cast<std::size_t>(ca->begin + under) * stride;
-      ca->end_skip = static_cast<std::size_t>(size + over - ca->end) * stride;
+      ca->begin_skip = static_cast<std::size_t>(ca->begin - start) * stride;
+      ca->end_skip = static_cast<std::size_t>(stop - ca->end) * stride;
       begin_.iter_ += ca->begin_skip;
 
-      stride *= size + under + over;
+      stride *= stop - start;
+      DEBUG(detail::get<0>(*r_begin));
+      DEBUG(ca->begin)
+      DEBUG(ca->begin_skip)
+      DEBUG(ca->end)
+      DEBUG(ca->end_skip)
+      DEBUG(stride)
       ++ca;
+      ++r_begin;
     });
   }
 
@@ -336,6 +359,22 @@ public:
   iterator end() noexcept { return end_; }
 
 private:
+  auto make_range(histogram_type& hist, coverage cov) {
+    using range_item = std::array<axis::index_type, 2>;
+    auto b = detail::make_stack_buffer<range_item>(unsafe_access::axes(hist));
+    hist.for_each_axis([cov, it = std::begin(b)](const auto& a) mutable {
+      (*it)[0] = 0;
+      (*it)[1] = a.size();
+      if (cov == coverage::all) {
+        (*it)[0] -= 1;
+        (*it)[1] += 1;
+      } else
+        assert(cov == coverage::inner);
+      ++it;
+    });
+    return b;
+  }
+
   iterator begin_, end_;
 };
 
@@ -362,8 +401,16 @@ private:
  */
 template <class Histogram>
 auto indexed(Histogram&& hist, coverage cov = coverage::inner) {
+  DEBUG("coverage");
   return indexed_range<std::remove_reference_t<Histogram>>{std::forward<Histogram>(hist),
                                                            cov};
+}
+
+template <class Histogram, class Iterable, class = detail::requires_iterable<Iterable>>
+auto indexed(Histogram&& hist, Iterable&& range) {
+  DEBUG("iterable");
+  return indexed_range<std::remove_reference_t<Histogram>>{std::forward<Histogram>(hist),
+                                                           std::forward<Iterable>(range)};
 }
 
 } // namespace histogram
