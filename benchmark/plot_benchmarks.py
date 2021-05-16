30e1a715718359e4c9e30f6936f13278b9013970
Hans Dembinski <hans.dembinski@gmail.com>
2021-05-16 11:51:38 +0200

    whitespace

--- benchmark/plot_benchmarks.py
+++ benchmark/plot_benchmarks.py
@@ -6,9 +6,6 @@
 
 from matplotlib import pyplot as plt, lines
 import shelve
-import json
-import subprocess as subp
-import sys
 from collections import defaultdict
 from run_benchmarks import get_commits, run
 import numpy as np
@@ -18,6 +15,7 @@ thread = None
 current_index = 0
 
 commits, comments = get_commits()
+
 
 def get_benchmarks(results):
     benchmarks = defaultdict(lambda: [])
@@ -30,6 +28,7 @@ def get_benchmarks(results):
                 benchmarks[name].append((commits.index(hash), time))
     return benchmarks
 
+
 with shelve.open("benchmark_results") as results:
     benchmarks = get_benchmarks(results)
 
@@ -38,7 +37,8 @@ plt.subplots_adjust(hspace=0, top=0.98, bottom=0.05, right=0.96)
 
 plt.sca(ax[0])
 for name, xy in benchmarks.items():
-    if "uniform" in name: continue
+    if "uniform" in name:
+        continue
     if "_1d" in name:
         x, y = np.transpose(xy)
         plt.plot(x, y, ".-", label=name)
@@ -46,7 +46,8 @@ plt.legend(fontsize="xx-small")
 
 plt.sca(ax[1])
 for name, xy in benchmarks.items():
-    if "uniform" in name: continue
+    if "uniform" in name:
+        continue
     if "_2d" in name:
         x, y = np.transpose(xy)
         plt.plot(x, y, ".-", label=name)
@@ -54,7 +55,8 @@ plt.legend(fontsize="xx-small")
 
 plt.sca(ax[2])
 for name, xy in benchmarks.items():
-    if "uniform" in name: continue
+    if "uniform" in name:
+        continue
     if "_3d" in name:
         x, y = np.transpose(xy)
         plt.plot(x, y, ".-", label=name)
@@ -62,13 +64,17 @@ plt.legend(fontsize="xx-small")
 
 plt.sca(ax[3])
 for name, xy in benchmarks.items():
-    if "uniform" in name: continue
+    if "uniform" in name:
+        continue
     if "_6d" in name:
         x, y = np.transpose(xy)
         plt.plot(x, y, ".-", label=name)
 plt.legend(fontsize="xx-small")
 
-plt.figtext(0.01, 0.5, "time per loop / ns [smaller is better]", rotation=90, va="center")
+plt.figtext(
+    0.01, 0.5, "time per loop / ns [smaller is better]", rotation=90, va="center"
+)
+
 
 def format_coord(x, y):
     global current_index
@@ -77,14 +83,18 @@ def format_coord(x, y):
     comment = comments[hash]
     return f"{hash} {comment}"
 
+
 for axi in ax.flatten():
     axi.format_coord = format_coord
 
+
 def on_key_press(event):
     global thread
-    if thread and thread.is_alive(): return
+    if thread and thread.is_alive():
+        return
 
-    if event.key != "u": return
+    if event.key != "u":
+        return
 
     hash = commits[current_index]
 
@@ -107,6 +117,7 @@ def on_key_press(event):
     thread = threading.Thread(target=worker, args=(fig, ax, hash))
     thread.start()
 
-fig.canvas.mpl_connect('key_press_event', on_key_press)
+
+fig.canvas.mpl_connect("key_press_event", on_key_press)
 
 plt.show()
--- benchmark/run_benchmarks.py
+++ benchmark/run_benchmarks.py
@@ -26,7 +26,6 @@ The script leaves the include folder in a modified state. To clean up, do:
 """
 import subprocess as subp
 import tempfile
-import os
 import shelve
 import json
 import argparse
@@ -35,12 +34,14 @@ import argparse
 def get_commits():
     commits = []
     comments = {}
-    for line in subp.check_output(("git", "log", "--oneline")).decode("ascii").split("\n"):
+    for line in (
+        subp.check_output(("git", "log", "--oneline")).decode("ascii").split("\n")
+    ):
         if line:
             ispace = line.index(" ")
             hash = line[:ispace]
             commits.append(hash)
-            comments[hash] = line[ispace+1:]
+            comments[hash] = line[ispace + 1 :]
     commits = commits[::-1]
     return commits, comments
 
@@ -72,7 +73,9 @@ def run(results, comments, hash, update):
             print(out.read().decode("utf-8") + "\n")
             return
     print(hash, "run")
-    s = subp.check_output(("./histogram_filling", "--benchmark_format=json", "--benchmark_filter=normal"))
+    s = subp.check_output(
+        ("./histogram_filling", "--benchmark_format=json", "--benchmark_filter=normal")
+    )
     d = json.loads(s)
     if update and hash in results and results[hash] is not None:
         d2 = results[hash]
@@ -86,14 +89,22 @@ def run(results, comments, hash, update):
 def main():
     commits, comments = get_commits()
 
-    parser = argparse.ArgumentParser(description=__doc__,
-                                     formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("first", type=str, default="begin",
-                        help="first commit in range, special value `begin` is allowed")
-    parser.add_argument("last", type=str, default="end",
-                        help="last commit in range, special value `end` is allowed")
-    parser.add_argument("-f", action="store_true",
-                        help="override previous results")
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "first",
+        type=str,
+        default="begin",
+        help="first commit in range, special value `begin` is allowed",
+    )
+    parser.add_argument(
+        "last",
+        type=str,
+        default="end",
+        help="last commit in range, special value `end` is allowed",
+    )
+    parser.add_argument("-f", action="store_true", help="override previous results")
 
     args = parser.parse_args()
 
@@ -106,11 +117,12 @@ def main():
         a = commits.index(args.first)
         b = commits.index(args.last)
         if args.f:
-            for hash in commits[a:b+1]:
+            for hash in commits[a : b + 1]:
                 del results[hash]
         run(results, comments, args.first, False)
         run(results, comments, args.last, False)
         recursion(results, commits, comments, a, b)
+
 
 if __name__ == "__main__":
     main()
