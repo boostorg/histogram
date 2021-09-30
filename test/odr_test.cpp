4f58af6b887169045d3980cd5384f022858ce2a6
Hans Dembinski <hans.dembinski@gmail.com>
2021-09-30 10:39:22 +0200

    fold the test of issue 342 into the odr test

--- test/odr_test.cpp
+++ test/odr_test.cpp
@@ -4,6 +4,11 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
+// The header windows.h and possibly others illegially do the following
+#define small char
+// which violates the C++ standard. We make sure here that including our headers work
+// nevertheless by avoid the name `small`.
+
 // include all Boost.Histogram header here; see odr_main_test.cpp for details
 #include <boost/histogram.hpp>
 #include <boost/histogram/ostream.hpp>
