61ecb19c67d48667e4aaa47047ac2679fe7b0567
Hans Dembinski <hans.dembinski@gmail.com>
2022-09-18 11:17:32 +0200

    docs for wald and wilson intervals

--- include/boost/histogram/utility/wald_interval.hpp
+++ include/boost/histogram/utility/wald_interval.hpp
@@ -16,6 +16,30 @@ namespace boost {
 namespace histogram {
 namespace utility {
 
+/**
+  Wald interval or normal approximation interval.
+
+  The Wald interval is an approximation based on the central limit
+  theorem, which is unsuitable when the sample size is small or when
+  the fraction is close to 0 or 1. In fact, the Wald interval has
+  poor statistical properties and should always be replaced by other
+  intervals, in particular the Wilson interval.
+
+  The Wald interval is commonly used by practitioners, since it can
+  be derived easily using the plug-in estimate of the variance for
+  the binomial distribution. Without further insight into statistical
+  theory, it is not clear that this derivation is flawed and that
+  better alternatives exist.
+
+  However, the Wald interval has many deficiencies that are avoided
+  by other intervals. It undercovers on average. Its limits are not
+  naturally bounded by 0 or 1. It produces empty intervals if the
+  number of successes or failures is zero.
+
+  For a critique of the Wald interval, see
+  R. D. Cousins, K. E. Hymes, J. Tucker,
+  Nucl. Instrum. Meth. A 612 (2010) 388-398.
+*/
 template <class ValueType>
 class wald_interval : public binomial_proportion_interval<ValueType> {
   using base_t = binomial_proportion_interval<ValueType>;
@@ -28,10 +52,14 @@ public:
       : z_{static_cast<value_type>(d)} {}
 
   interval_type operator()(value_type successes, value_type failures) const noexcept {
+    // See https://en.wikipedia.org/wiki/
+    //   Binomial_proportion_confidence_interval
+    //   #Normal_approximation_interval_or_Wald_interval
     const value_type s = successes, f = failures;
     const value_type n = s + f;
-    const value_type a = s / n;
-    const value_type b = (z_ / n) * std::sqrt(s * f / n);
+    const value_type ninv = 1 / n;
+    const value_type a = s * ninv;
+    const value_type b = (z_ * ninv) * std::sqrt(s * f * ninv);
     return std::make_pair(a - b, a + b);
   }
 
--- include/boost/histogram/utility/wilson_interval.hpp
+++ include/boost/histogram/utility/wilson_interval.hpp
@@ -17,12 +17,22 @@ namespace histogram {
 namespace utility {
 
 /**
-  Wilson score interval.
+  Wilson interval.
 
-  TODO (Hans) explain benefits of this interval here.
+  The Wilson score interval is simple to compute, has good coverage. Intervals are
+  automatically bounded between 0 and 1 and never empty. The interval is asymmetric.
 
-  Source:
-  https://en.wikipedia.org/wiki/Binomial_proportion_confidence_interval#Wilson_score_interval
+  Wilson, E. B. (1927). "Probable inference, the law of succession, and statistical
+  inference". Journal of the American Statistical Association. 22 (158): 209-212.
+  doi:10.1080/01621459.1927.10502953. JSTOR 2276774.
+
+  The coverage probability is close to the nominal value. Unlike the Clopper-Pearson
+  interval, it is not conservative. For some values of the fractions, the interval
+  undercovers and overcovers for neighboring values. On "average", the intervals have
+  close to nomimal coverage. The Wilson score intervals and the Clopper-Pearson are
+  recommended for general use (the second if the interval has to be conservative)
+  in a review of the existing literature by R. D. Cousins, K. E. Hymes, J. Tucker,
+  Nucl. Instrum. Meth. A 612 (2010) 388-398.
 */
 template <class ValueType>
 class wilson_interval : public binomial_proportion_interval<ValueType> {
@@ -36,6 +46,14 @@ public:
       : z_{static_cast<value_type>(d)} {}
 
   interval_type operator()(value_type successes, value_type failures) const noexcept {
+    // See https://en.wikipedia.org/wiki/
+    //   Binomial_proportion_confidence_interval
+    //   #Wilson_score_interval
+
+    // We make sure calculation is done in single precision if value_type is float
+    // by converting all literal constants to value_type. If the literals remain
+    // in the equation, they turn the calculation of intermediate falues to double,
+    // since mixed arithemetic between float and double results in double.
     const value_type half{0.5}, quarter{0.25}, zsq{z_ * z_};
     const value_type s = successes, f = failures;
     const value_type n = s + f;
