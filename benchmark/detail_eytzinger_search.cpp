8ab311f000c2c0e363ebecf5db18495707b25853
Hans Dembinski <hans.dembinski@gmail.com>
2022-12-23 11:30:34 +0100

    support prefetch

--- benchmark/detail_eytzinger_search.cpp
+++ benchmark/detail_eytzinger_search.cpp
@@ -5,16 +5,17 @@
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <benchmark/benchmark.h>
+#include <algorithm>
+#include <boost/core/bit.hpp>
 #include <boost/histogram/axis/variable.hpp>
+#include <boost/histogram/detail/prefetch.hpp>
+#include <iostream>
 #include <numeric>
+#include <vector>
 #include "../test/throw_exception.hpp"
 #include "generator.hpp"
-#include <algorithm>
-#include <vector>
-#include <iostream>
-#include <boost/core/bit.hpp>
 #ifdef HAVE_BOOST_ALIGN
-  #include <boost/align/aligned_allocator.hpp>
+#include <boost/align/aligned_allocator.hpp>
 #endif
 #include <boost/core/lightweight_test.hpp>
 
@@ -25,63 +26,66 @@ struct assert_check {
   }
 } _;
 
-template<typename T=double>
-struct eytzinger_search {
+using namespace boost::histogram;
 
-  int ffs(size_t v) const noexcept
-  {
-      if(v==0) return 0;
-  #if HAVE_BOOST_MULTIPRECISION
-      return boost::multiprecision::lsb(v)+1;
-  #else
-      // we prefer boost::core since it is a dependency already
-      return boost::core::countr_zero(v)+1;
-  #endif
+template <typename T = double>
+struct eytzinger_search {
+  int ffs(size_t v) const noexcept {
+    if (v == 0) return 0;
+#if HAVE_BOOST_MULTIPRECISION
+    return boost::multiprecision::lsb(v) + 1;
+#else
+    // we prefer boost::core since it is a dependency already
+    return boost::core::countr_zero(v) + 1;
+#endif
   }
 
-  eytzinger_search(const std::vector<T>& a) :
-      b_(a.size() + 1), idx_(a.size() + 1)
-  {
-      init(a);
-      idx_[0] = a.size() - 1;
+  eytzinger_search(const std::vector<T>& a) : b_(a.size() + 1), idx_(a.size() + 1) {
+    init(a);
+    idx_[0] = a.size() - 1;
   }
 
   int index(T const& x) const {
-      size_t k = 1;
-      while (k < b_.size())
-          k = 2 * k + (b_[k] < x);
-      k >>= ffs(~k);
-      return idx_[k];
+    size_t k = 1;
+    while (k < b_.size()) {
+      constexpr int block_size = detail::cacheline_length / sizeof(T);
+      detail::prefetch(b_.data() + k * block_size);
+      k = 2 * k + !(x < b_[k]); // double negation to handle nan correctly
+    }
+    k >>= ffs(~k);
+    return idx_[k];
   }
 
   size_t init(const std::vector<T>& a, size_t i = 0, size_t k = 1) {
-      if (k <= a.size()) {
-          i = init(a, i, 2 * k);
-          idx_[k] = i - 1;
-          b_[k] = a[i++];
-          i = init(a, i, 2 * k + 1);
-      }
-      return i;
+    if (k <= a.size()) {
+      i = init(a, i, 2 * k);
+      idx_[k] = i - 1;
+      b_[k] = a[i++];
+      i = init(a, i, 2 * k + 1);
+    }
+    return i;
   }
 
 #ifdef HAVE_BOOST_ALIGN
-  std::vector<T, boost::alignment::aligned_allocator<T, 64>> b_;
-  std::vector<int, boost::alignment::aligned_allocator<int, 64>> idx_;
+  std::vector<T, boost::alignment::aligned_allocator<T, detail::cache_alignment>> b_;
+  std::vector<int, boost::alignment::aligned_allocator<int, detail::cache_alignment>>
+      idx_;
 #else
   std::vector<T> b_;
   std::vector<int> idx_;
 #endif
 };
 
-using namespace boost::histogram;
-
 template <class Distribution>
 static void variable(benchmark::State& state) {
   std::vector<double> v;
   for (double x = 0; x <= state.range(0); ++x) { v.push_back(x / state.range(0)); }
   auto a = axis::variable<>(v);
-  generator<Distribution> gen;  
-  for (auto _ : state) benchmark::DoNotOptimize(a.index(gen()));
+  generator<Distribution> gen;
+  std::vector<double> x(10000);
+  std::generate(x.begin(), x.end(), gen);
+  unsigned i = 0;
+  for (auto _ : state) benchmark::DoNotOptimize(a.index(x[i++ % x.size()]));
 }
 
 template <class Distribution>
@@ -89,28 +93,36 @@ static void eytzinger(benchmark::State& state) {
   std::vector<double> v;
   for (double x = 0; x <= state.range(0); ++x) { v.push_back(x / state.range(0)); }
   auto a = eytzinger_search<double>(v);
-  generator<Distribution> gen;    
+  generator<Distribution> gen;
 
   auto aref = axis::variable<>(v);
-  for (int i = 0; i < 10000; ++i) {
-    const double x = gen();
+  std::vector<double> inputs(1000);
+  std::generate(inputs.begin(), inputs.end(), gen);
+  inputs[0] = std::numeric_limits<double>::quiet_NaN();
+  inputs[1] = -std::numeric_limits<double>::infinity();
+  inputs[2] = std::numeric_limits<double>::infinity();
+  inputs[3] = v[0];
+  inputs[4] = v.back();
+  for (auto&& x : inputs) {
     if (a.index(x) != aref.index(x)) {
-      std::cout << "x = " << x << " " 
-                << a.index(x) << " " 
-                << aref.index(x) << std::endl;
+      std::cout << "x = " << x << ": new = " << a.index(x) << " ref = " << aref.index(x)
+                << std::endl;
       std::abort();
     }
   }
 
-  for (auto _ : state) benchmark::DoNotOptimize(a.index(gen()));
+  std::vector<double> x(10000);
+  std::generate(x.begin(), x.end(), gen);
+  unsigned i = 0;
+  for (auto _ : state) benchmark::DoNotOptimize(a.index(x[i++ % x.size()]));
 }
 
-BENCHMARK_TEMPLATE(variable, uniform)->RangeMultiplier(10)->Range(10, 10000);
-BENCHMARK_TEMPLATE(variable, normal)->RangeMultiplier(10)->Range(10, 10000);
-BENCHMARK_TEMPLATE(variable, chi2)->RangeMultiplier(10)->Range(10, 10000);
-BENCHMARK_TEMPLATE(variable, expon)->RangeMultiplier(10)->Range(10, 10000);
+BENCHMARK_TEMPLATE(eytzinger, uniform)->RangeMultiplier(10)->Range(10, 100000);
+BENCHMARK_TEMPLATE(eytzinger, normal)->RangeMultiplier(10)->Range(10, 100000);
+BENCHMARK_TEMPLATE(eytzinger, chi2)->RangeMultiplier(10)->Range(10, 100000);
+BENCHMARK_TEMPLATE(eytzinger, expon)->RangeMultiplier(10)->Range(10, 100000);
 
-BENCHMARK_TEMPLATE(eytzinger, uniform)->RangeMultiplier(10)->Range(10, 10000);
-BENCHMARK_TEMPLATE(eytzinger, normal)->RangeMultiplier(10)->Range(10, 10000);
-BENCHMARK_TEMPLATE(eytzinger, chi2)->RangeMultiplier(10)->Range(10, 10000);
-BENCHMARK_TEMPLATE(eytzinger, expon)->RangeMultiplier(10)->Range(10, 10000);
+BENCHMARK_TEMPLATE(variable, uniform)->RangeMultiplier(10)->Range(10, 100000);
+BENCHMARK_TEMPLATE(variable, normal)->RangeMultiplier(10)->Range(10, 100000);
+BENCHMARK_TEMPLATE(variable, chi2)->RangeMultiplier(10)->Range(10, 100000);
+BENCHMARK_TEMPLATE(variable, expon)->RangeMultiplier(10)->Range(10, 100000);
---
+++ include/boost/histogram/detail/prefetch.hpp
@@ -0,0 +1,54 @@
+//          Copyright Oliver Kowalke, Hans Dembinski 2014-2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_HISTOGRAM_DETAIL_PREFETCH_H
+#define BOOST_HISTOGRAM_DETAIL_PREFETCH_H
+
+#include <boost/config.hpp>
+#include <cstddef>
+#include <cstdint>
+
+#if BOOST_GCC || BOOST_CLANG
+#define BOOST_HISTOGRAM_DETAIL_HAVE_BUILTIN_PREFETCH
+#elif BOOST_INTEL
+#define BOOST_HISTOGRAM_DETAIL_HAVE_MM_PREFETCH
+#include <immintrin.h>
+#elif BOOST_MSVC && !defined(_M_ARM) && !defined(_M_ARM64)
+#define BOOST_HISTOGRAM_DETAIL_HAVE_MM_PREFETCH
+#include <mmintrin.h>
+#endif
+
+namespace boost {
+namespace histogram {
+namespace detail {
+
+// modern architectures have cachelines with 64byte length
+// ARM Cortex-A15 32/64byte, Cortex-A9 16/32/64bytes
+// MIPS 74K: 32byte, 4KEc: 16byte
+// ist should be safe to use 64byte for all
+static constexpr std::size_t cache_alignment{64};
+static constexpr std::size_t cacheline_length{64};
+// lookahead size for prefetching
+static constexpr std::size_t prefetch_stride{4 * cacheline_length};
+
+BOOST_FORCEINLINE
+void prefetch(const void* addr) {
+#if defined BOOST_HISTOGRAM_DETAIL_HAVE_MM_PREFETCH
+  _mm_prefetch((const char*)addr, _MM_HINT_T0);
+#elif defined BOOST_HISTOGRAM_DETAIL_HAVE_BUILTIN_PREFETCH
+  // L1 cache : hint == 1
+  __builtin_prefetch((void*)addr, 1, 1);
+#endif
+  // no prefetch available
+}
+
+#undef BOOST_HISTOGRAM_DETAIL_HAVE_MM_PREFETCH
+#undef BOOST_HISTOGRAM_DETAIL_HAVE_BUILTIN_PREFETCH
+
+} // namespace detail
+} // namespace histogram
+} // namespace boost
+
+#endif // BOOST_HISTOGRAM_DETAIL_PREFETCH_H
