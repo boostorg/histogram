3be56fc1cb5371da79b1406ee4915202c8234d70
Hans Dembinski <hans.dembinski@gmail.com>
2022-12-26 11:08:00 +0100

    try to catch for illegal min,max macros

--- test/odr_test.cpp
+++ test/odr_test.cpp
@@ -4,11 +4,13 @@
 // (See accompanying file LICENSE_1_0.txt
 // or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// The header windows.h and possibly others illegially do the following
-#define small char
+// The header windows.h and possibly others illegially use unprotected defines
+#define small macro_substitution_this_shouldn_t_be_happening
+#define min(A, B) macro_substitution_this_shouldn_t_be_happening
+#define max(A, B) macro_substitution_this_shouldn_t_be_happening
 // which violates the C++ standard. We make sure here that including our headers work
-// nevertheless by avoiding the preprocessing token `small`. For more details, see
-// https://github.com/boostorg/histogram/issues/342
+// nevertheless by avoiding these preprocessing tokens or by preventing their macro
+// substitution. For more details, see https://github.com/boostorg/histogram/issues/342
 
 // include all Boost.Histogram header here; see odr_main_test.cpp for details
 #include <boost/histogram.hpp>
