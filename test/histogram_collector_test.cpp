0816a5ba12937079c94667556220931dc53322c4
Hans Dembinski <hans.dembinski@gmail.com>
2024-04-24 09:15:25 +0200

    test more thorough and minimal

--- test/histogram_collector_test.cpp
+++ test/histogram_collector_test.cpp
@@ -1,3 +1,9 @@
+// Copyright 2024 Ruggero Turra, Hans Dembinski
+//
+// Distributed under the Boost Software License, version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/core/lightweight_test.hpp>
 #include <boost/histogram/accumulators/collector.hpp>
 #include <boost/histogram/accumulators/ostream.hpp>
@@ -24,21 +30,20 @@ int main() {
   BOOST_TEST_EQ(h.at(1).count(), 1);
   BOOST_TEST_EQ(h.at(2).count(), 0);
 
-  BOOST_TEST_EQ(h.at(0)[0], 1.1);
-  BOOST_TEST_EQ(h.at(0)[1], 2.2);
-  BOOST_TEST_EQ(h.at(1)[0], 10.10);
   BOOST_TEST_EQ(h.at(0), collector_t({1.1, 2.2}));
+  BOOST_TEST_EQ(h.at(1)[0], 10.10);
 
-  std::vector<int> x = {0, 0, 0, 0, 0};
+  std::vector<int> x = {0, 1, 0, 2, 0};
   std::array<double, 5> data = {-1.1, -1.2, -1.3, -1.4, -1.5};
   h.fill(x, sample(data));
 
-  BOOST_TEST_EQ(h.at(0).count(), 7);
-  BOOST_TEST_EQ(h.at(1).count(), 1);
-  BOOST_TEST_EQ(h.at(2).count(), 0);
+  BOOST_TEST_EQ(h.at(0).count(), 5);
+  BOOST_TEST_EQ(h.at(1).count(), 2);
+  BOOST_TEST_EQ(h.at(2).count(), 1);
   BOOST_TEST_NE(h.at(0), collector_t({1.1, 2.2}));
-  BOOST_TEST_EQ(h.at(0), collector_t({1.1, 2.2, -1.1, -1.2, -1.3, -1.4, -1.5}));
-  BOOST_TEST_EQ(h.at(1), collector_t({10.10}));
+  BOOST_TEST_EQ(h.at(0), collector_t({1.1, 2.2, -1.1, -1.3, -1.5}));
+  BOOST_TEST_EQ(h.at(1), collector_t({10.10, -1.2}));
+  BOOST_TEST_EQ(h.at(2), collector_t({-1.4}));
 
   return boost::report_errors();
 }
