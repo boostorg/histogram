87569f4c0efd3148d64113d833ba517209452e57
Hans Dembinski <hans.dembinski@gmail.com>
2024-04-24 09:07:55 +0200

    add copyright notice

--- test/accumulators_collector_test.cpp
+++ test/accumulators_collector_test.cpp
@@ -1,3 +1,9 @@
+// Copyright 2024 Ruggero Turra, Hans Dembinski
+//
+// Distributed under the Boost Software License, version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #include <array>
 #include <boost/core/lightweight_test.hpp>
 #include <boost/core/span.hpp>
